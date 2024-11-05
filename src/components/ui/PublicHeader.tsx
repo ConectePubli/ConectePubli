271a72855596205f614676f99bdd1f5fe836bb66
CarlosHenrique7777 <carloshenrique.webdev@gmail.com>
2024-11-05 12:48:30 -0300

    fix: minor fix

--- src/components/ui/PublicHeader.tsx
+++ src/components/ui/PublicHeader.tsx
@@ -9,17 +9,19 @@ export const PublicHeader = () => {
   const navigate = useNavigate();
 
   return (
-    <header className="bg-white h-[66px] flex justify-between items-center px-4 border-b-[1px]">
-      <Link to="/">
-        <img src={logo} alt="ConectePubli" className="h-10" />
-      </Link>
-      <Button
-        variant="orange"
-        size="default"
-        onClick={() => navigate({ to: "/cadastro" })}
-      >
-        Fazer Pré Cadastro
-      </Button>
+    <header className="bg-white border-b-[1px]">
+      <div className="h-[66px] flex justify-between items-center mx-auto max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl px-4">
+        <Link to="/">
+          <img src={logo} alt="ConectePubli" className="h-10" />
+        </Link>
+        <Button
+          variant="orange"
+          size="default"
+          onClick={() => navigate({ to: "/cadastro" })}
+        >
+          Fazer Pré Cadastro
+        </Button>
+      </div>
     </header>
   );
 };
