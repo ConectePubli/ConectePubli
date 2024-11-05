ac5511da9ececc58b01ce6d2ad65cdac5544be96
CarlosHenrique7777 <carloshenrique.webdev@gmail.com>
2024-11-05 11:35:19 -0300

    feat: added footer logo redirect

--- src/components/sections/LandingPage/Footer.tsx
+++ src/components/sections/LandingPage/Footer.tsx
@@ -6,7 +6,13 @@ export const Footer: React.FC = () => {
     <footer className="py-4 border-t bg-white">
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
         <div className="flex items-center">
-          <img src={logo} alt="ConectePubli Logo" className="h-10" />
+          <img
+            src={logo}
+            alt="ConectePubli Logo"
+            className="h-8 md:h-10 cursor-pointer"
+            onClick={() => window.location.replace("/")}
+            draggable={false}
+          />
         </div>
 
         <div className="mt-4 md:mt-0 text-center md:text-right text-gray-600">
