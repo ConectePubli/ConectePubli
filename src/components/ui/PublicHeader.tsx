36b77cd0538b831736d3fdb8fe27bc048bc85440
CarlosHenrique7777 <carloshenrique.webdev@gmail.com>
2024-11-06 08:08:19 -0300

    fix: enhance responsiveness for headers

--- src/components/ui/PublicHeader.tsx
+++ src/components/ui/PublicHeader.tsx
@@ -10,7 +10,7 @@ export const PublicHeader = () => {
 
   return (
     <header className="bg-white border-b-[1px]">
-      <div className="h-[66px] flex justify-between items-center mx-auto max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl px-4">
+      <div className="py-4 flex flex-wrap gap-4 justify-between items-center mx-auto max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl px-4">
         <Link to="/">
           <img src={logo} alt="ConectePubli" className="h-10" />
         </Link>
