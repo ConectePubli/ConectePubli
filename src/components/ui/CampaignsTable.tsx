1d714f1d23596c4fdb0d4abfddeeeac366d698df
Guilherme Guidetti <53499339+guilhermeguidetti@users.noreply.github.com>
2025-02-06 11:50:05 -0300

    feat: add campaign duplication functionality to CampaignsTable

--- src/components/ui/CampaignsTable.tsx
+++ src/components/ui/CampaignsTable.tsx
@@ -3,12 +3,73 @@ import { format } from "date-fns";
 import { useNavigate } from "@tanstack/react-router";
 import { formatDateUTC } from "@/utils/formatDateUTC";
 import { useTranslation } from "react-i18next";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "./dropdown-menu";
+import { Ellipsis } from "lucide-react";
+import { Button } from "./button";
+import { Campaign } from "@/types/Campaign";
+import pb from "@/lib/pb";
+import { generateUniqueName } from "@/services/createCampaign";
+import { toast } from "react-toastify";
 
 const CampaignsTable: React.FC = () => {
   const { t } = useTranslation();
   const { campaigns, isLoading, error } = useCampaignStore();
 
   const navigate = useNavigate();
+
+  const handleDuplicateCampaign = async (originalCampaign: Campaign) => {
+    if (originalCampaign.status !== "ready") {
+      toast.error(t("Somente campanhas prontas podem ser duplicadas."));
+      return;
+    }
+    try {
+      // Gerar unique_name único
+      const existingUniqueNames: string[] = await pb
+        .collection("Campaigns")
+        .getFullList({ fields: "unique_name" })
+        .then((campaigns) => campaigns.map((c) => c.unique_name));
+
+      const baseName = originalCampaign.name
+        .replace(/[^\p{L}\p{N}\s]/gu, "")
+        .toLowerCase()
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .trim()
+        .split(/\s+/)
+        .slice(0, 5)
+        .join("_");
+
+      const newUniqueName = generateUniqueName(baseName, existingUniqueNames);
+
+      const newDraftData = {
+        ...originalCampaign,
+        id: undefined,
+        created: undefined,
+        updated: undefined,
+        collectionId: undefined,
+        collectionName: undefined,
+        status: "draft",
+        unique_name: newUniqueName,
+      };
+
+      const newCampaign = await pb.collection("Campaigns").create(newDraftData);
+
+      navigate({
+        to: "/dashboard-marca/criar-campanha",
+        search: {
+          campaign_id: newCampaign.id,
+          is_draft: true,
+        },
+      });
+    } catch (error) {
+      console.error("Erro ao duplicar campanha:", error);
+      toast.error(t("Erro ao duplicar campanha"));
+    }
+  };
 
   return (
     <div>
@@ -33,21 +94,24 @@ const CampaignsTable: React.FC = () => {
             <th className="py-3 px-4 text-left whitespace-nowrap">
               {t("Início/Fim Campanha")}
             </th>
-            <th className="py-3 px-4 text-left whitespace-nowrap rounded-tr-lg">
+            <th className="py-3 px-4 text-left whitespace-nowrap">
               {t("Status")}
+            </th>
+            <th className="py-3 px-4 text-left whitespace-nowrap rounded-tr-lg">
+              {t("Ações")}
             </th>
           </tr>
         </thead>
         <tbody className="border border-gray-200 border-t-0">
           {isLoading ? (
             <tr>
-              <td colSpan={7} className="py-4 px-4 text-center text-gray-500">
+              <td colSpan={8} className="py-4 px-4 text-center text-gray-500">
                 {t("Carregando campanhas...")}
               </td>
             </tr>
           ) : error ? (
             <tr>
-              <td colSpan={7} className="py-4 px-4 text-center text-red-500">
+              <td colSpan={8} className="py-4 px-4 text-center text-red-500">
                 {error}
               </td>
             </tr>
@@ -56,11 +120,11 @@ const CampaignsTable: React.FC = () => {
               const participations =
                 campaign.expand?.Campaigns_Participations_via_campaign || [];
 
-              const inscritos = participations.length; // Quantidade de inscritos
+              const inscritos = participations.length;
 
               const aprovados = participations.filter(
                 (p) => p.status === "approved" || p.status === "completed"
-              ).length; // Quantidade de aprovados
+              ).length;
 
               return (
                 <tr
@@ -143,12 +207,33 @@ const CampaignsTable: React.FC = () => {
                               ? t("Campanha recusada")
                               : ""}
                   </td>
+
+                  <td
+                    className="py-2 px-4 font-semibold"
+                    onClick={(e) => e.stopPropagation()}
+                  >
+                    <DropdownMenu>
+                      <DropdownMenuTrigger>
+                        <Ellipsis />
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent>
+                        <Button
+                          variant="blue"
+                          size="sm"
+                          className="w-full"
+                          onClick={() => handleDuplicateCampaign(campaign)}
+                        >
+                          Duplicar Campanha
+                        </Button>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </td>
                 </tr>
               );
             })
           ) : (
             <tr>
-              <td colSpan={7} className="py-4 px-4 text-center text-gray-500">
+              <td colSpan={8} className="py-4 px-4 text-center text-gray-500">
                 {t("Nenhuma campanha disponível.")}
               </td>
             </tr>
